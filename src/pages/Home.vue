24b95db6d092b6ba52d9669c07922bc9accb4462
Lucas Monteiro <lucas.monteiro58@gmail.com>
2021-05-09 09:29:59 -0300

    initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div class="logo-game"></div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.container {
+  @include flex-center;
+  flex-direction: column;
+}
+</style>
